1bc0f78b281ff47ed4ef50f1918f554792c34f36
niro-pathi <niroshana.pathirana@outlook.com>
2024-07-20 19:53:50 +1000

    Added working with working with valid identifiers in C# and Primitive types

---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Heavenly Scent's Online Shop");
+
+Console.WriteLine("Enter the candle type you need:");
+
+//The following will accept the type of the candle
+string candleType = Console.ReadLine();
+
+/*
+    Here are some tests around working with valid identifiers in C#
+    We can write here as much as we want, this is all comment
+*/
+
+Console.WriteLine("Enter number of candles you need:");
+var numberOfCandles = int.Parse(Console.ReadLine());
+
+const double candlePrice = 49.99;
+
+double totalPrice = numberOfCandles * candlePrice;
+
+Console.WriteLine(totalPrice);
+
