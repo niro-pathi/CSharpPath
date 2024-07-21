130ff087f20a96856d96a4d30e6c79cee8b4eca0
niro-pathi <niroshana.pathirana@outlook.com>
2024-07-21 11:27:15 +1000

    Creating and using methods.

--- CSharpFundamentals L03/Program.cs
+++ CSharpFundamentals L03/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+ * Working with methods
+ *  - Creating and using methods
+ */
+
+/*
+ * Creating and using methods
+ *  - creating a method
+ *  - adding parameters
+ *  - returning a value
+ *  - invoking a method
+ */
+
+using System.Net.NetworkInformation;
+
+Console.WriteLine("Enter the quantity you want to order : ");
+int orderQty = int.Parse(Console.ReadLine());
+double totalPrice = CalculatePrice(orderQty);
+string deliveryDate = DeliveryDate();
+
+Console.WriteLine($"Your order price is ${totalPrice} and will be delivered on {deliveryDate}");
+
+static double CalculatePrice (int qty)
+{
+    double unitPrice = 49.99;
+
+    return qty * unitPrice;
+}
+
+static string DeliveryDate() 
+{  
+    DateTime deliveryDate = DateTime.Now;
+    return deliveryDate.ToLongDateString(); 
+}
